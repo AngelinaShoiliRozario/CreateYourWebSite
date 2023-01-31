1ad5ce672c11755827ae5b791b6d6b0313da98da
AngelinaShoiliRozario <shoili.rozario@gmail.com>
2023-02-01 01:29:08 +0600

    add image and text edit

---
+++ resources/views/components/image_background_adder.blade.php
@@ -0,0 +1,34 @@
+<div class="image_uploader">
+    <div class="text-center p-3">Background</div>
+
+    <div class="dropdown " style="width: 300px">
+        <div class="img_preview d-flex flex-wrap">
+            <img src="./assets/images/banner/banner-1-bg.jpg" alt="" style="width: 33.3333%; height: auto" />
+            <img src="./assets/images/banner/banner-3.png" alt="" style="width: 33.3333%; height: auto" />
+            <img src="./assets/images/banner/banner-1-bg.jpg" alt="" style="width: 33.3333%; height: auto" />
+            <img src="./assets/images/banner/banner-3.png" alt="" style="width: 33.3333%; height: auto" />
+            <img src="./assets/images/banner/banner-1-bg.jpg" alt="" style="width: 33.3333%; height: auto" />
+            <img src="./assets/images/banner/banner-3.png" alt="" style="width: 33.3333%; height: auto" />
+        </div>
+        <button type="button" class="btn mt-1 w-100 btn-outline-info" data-bs-toggle="modal"
+            data-bs-target="#staticBackdrop">
+            upload image
+        </button>
+        <select class="form-select select2">
+            <option value="tile" selected>tile</option>
+            <option value="center">Center</option>
+
+            <option value="strech">Strech</option>
+
+            <option value="fit">fit</option>
+
+        </select>
+        <button type="button" class="btn mt-1 w-100 btn-outline-danger" data-bs-toggle="modal"
+            data-bs-target="#staticBackdrop">
+            Remove
+        </button><button type="button" class="btn mt-1 w-100 btn-outline-success mb-2" data-bs-toggle="modal"
+            data-bs-target="#staticBackdrop">
+            Save
+        </button>
+    </div>
+</div>
---
+++ resources/views/components/upload_img_modal.blade.php
@@ -0,0 +1,27 @@
+ <!-- modal start -->
+ <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1"
+     aria-labelledby="staticBackdropLabel" aria-hidden="true">
+     <div class="modal-dialog modal-dialog-centered modal-lg">
+         <div class="modal-content">
+             <div class="modal-header">
+                 <h5 class="modal-title" id="staticBackdropLabel">
+                     Upload image here
+                 </h5>
+                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+             </div>
+             <div class="modal-body">
+                 {{-- content --}}
+                 <input type="file" name="" id="" onchange="preview_image(this)">
+             </div>
+             <div class="modal-footer">
+                 <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">
+                     Close
+                 </button>
+                 <button type="button" class="btn btn-primary">
+                     Understood
+                 </button>
+             </div>
+         </div>
+     </div>
+ </div>
+ <!-- modal end -->
